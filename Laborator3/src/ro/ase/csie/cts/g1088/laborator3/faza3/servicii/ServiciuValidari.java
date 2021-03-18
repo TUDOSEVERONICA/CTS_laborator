e562fb551f5d3fc12603ec39d3df6bbefa8c1624
TUDOSEVERONICA <72127984+TUDOSEVERONICA@users.noreply.github.com>
2021-03-18 12:26:22 +0200

    commit corect

--- Laborator3/src/ro/ase/csie/cts/g1088/laborator3/faza3/servicii/ServiciuValidari.java
+++ Laborator3/src/ro/ase/csie/cts/g1088/laborator3/faza3/servicii/ServiciuValidari.java
@@ -1,5 +1,22 @@
 package ro.ase.csie.cts.g1088.laborator3.faza3.servicii;
 
-public class ServiciuValidari implements InterfataValidare {
+import ro.ase.csie.cts.g1088.laborator3.exceptii.ExceptiePretInvalid;
+import ro.ase.csie.cts.g1088.laborator3.exceptii.ExceptieVechimeClient;
+
+public class ServiciuValidari implements InterfataValidare{
+
+	@Override
+	public void validarePret(float pretInitial) throws ExceptiePretInvalid {
+		if(pretInitial <= 0) {
+			throw new ExceptiePretInvalid();
+		}
+	}
+
+	@Override
+	public void validareVechimeClient(int vechimeClientInAni) throws ExceptieVechimeClient {
+		if(vechimeClientInAni < 0) {
+			throw new ExceptieVechimeClient();
+		}
+	}
 
 }
--- Laborator3/src/ro/ase/csie/cts/g1088/laborator3/faza3/servicii/StrategieMarketing2021.java
+++ Laborator3/src/ro/ase/csie/cts/g1088/laborator3/faza3/servicii/StrategieMarketing2021.java
@@ -1,23 +1,11 @@
 package ro.ase.csie.cts.g1088.laborator3.faza3.servicii;
 
-import ro.ase.csie.cts.g1088.laborator3.exceptii.ExceptiePretInvalid;
-import ro.ase.csie.cts.g1088.laborator3.exceptii.ExceptieVechimeClient;
+public class StrategieMarketing2021 implements InterfataMarketing{
 
-public class StrategieMarketing2021 implements InterfataMarketing {
 	@Override
 	public float getDiscountFidelitate(int vechimeClientInAni) {
-		return (vechimeClientInAni > VECHIME_CLIENT_MAXIMA) ? DISCOUNT_CLIENT_MAXIM : (float)vechimeClientInAni/100;
+		return (vechimeClientInAni > VECHIME_CLIENT_MAXIMA) ? 
+				DISCOUNT_CLIENT_MAXIM : (float)vechimeClientInAni/100;
 	}
-	
-	@Override
-	public void validarePret(float pretInitial) throws ExceptiePretInvalid{
-		if(pretInitial<=0) {
-			throw new ExceptiePretInvalid();
-		}
-	}
-	
-	@Override
-	public void validareVechimeClient(int vechimeClientInAni) throws ExceptieVechimeClient{
-		
-	}
+
 }
